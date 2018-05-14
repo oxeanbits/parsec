06c3aa99b79e08c57318565e8e0297f65351d255
Beatriz Fagundes <beatrizsfslima@gmail.com>
2018-05-14 19:22:36 -0300

    Create validate_syntax function and start tests

--- lib/parsec.rb
+++ lib/parsec.rb
@@ -4,16 +4,24 @@ module EquationsParser
   class Parsec
     using StringToBooleanRefinements
 
-    def eval(equation)
+    def eval_equation(equation)
       # This line removes all spaces that are not between quotation marks
       # https://stackoverflow.com/questions/205521/using-regex-to-replace-all-spaces-not-in-quotes-in-ruby?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
       equation.gsub!(/( |(".*?"))/, "\\2")
 
-      output = `./muparsex-bin-macOS <<< '#{equation}'`
+      output = `./bin/muparserx-macOS <<< '#{equation}'`
       result = output.split('parsec> ', 2).last
       result_type = result.split('type: \'', 2).last[0]
       result_value = result.split('ans = ', 2).last.strip
       convert(result_value, result_type)
+    end
+
+    def validate_syntax(equation)
+      # Regex that validates if the equation represents the syntax expected by murparsex
+      # THE REGEX IS INCOMPLETE!
+      regex_syntax = %r{^\s*-?\d+(?:\s*[-+*/^]\s*\d+)+$}
+      raise StandardError, 'Wrong formula syntax' unless equation =~ regex_syntax
+      true
     end
 
     private
--- test/test_parsec.rb
+++ test/test_parsec.rb
@@ -1,4 +1,9 @@
-require 'helper'
+require 'minitest/autorun'
+require 'parsec'
 
 class TestParsec < Minitest::Test
+  def eval_correct_equations
+    assert_equal(10, eval('5*2'))
+    assert_equal(25, eval('5^2'))
+  end
 end

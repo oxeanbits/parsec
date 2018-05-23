f001e0b72b4fcee2937cbd10b542af38f285e25b
Beatriz Fagundes <beatrizsfslima@gmail.com>
2018-05-23 08:54:26 -0300

    Change blank? to nil? or strip.empty?

--- lib/string_to_boolean_refinements.rb
+++ lib/string_to_boolean_refinements.rb
@@ -3,7 +3,7 @@ module StringToBooleanRefinements
   refine String do
     def to_bool
       return true  if self == true  || self =~ /(true|t|yes|y|1|on)$/i
-      return false if self == false || blank? || self =~ /(false|f|no|n|0|off)$/i
+      return false if self == false || nil? || strip.empty? || self =~ /(false|f|no|n|0|off)$/i
       raise ArgumentError, "invalid value for Boolean: \"#{self}\""
     end
   end

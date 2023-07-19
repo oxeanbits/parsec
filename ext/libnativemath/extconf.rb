1d119e079fd644be45383f1c96894767b63eeb51
Victor Cordeiro Costa <victorcorcos@gmail.com>
2023-07-19 17:14:20 -0300

    Change wrong ENV variable comparison

--- ext/libnativemath/extconf.rb
+++ ext/libnativemath/extconf.rb
@@ -73,7 +73,8 @@ end
 Dir.chdir(BASEDIR) do
   Dir.chdir('ext/libnativemath/') do
     system('make')
-    next if OSTYPE.match? 'darwin'
+
+    next if RUBY_PLATFORM.match? 'darwin'
 
     if RUBY_VERSION >= '3.2'
       FileUtils.cp('libnativemath.so', '../../lib/')
